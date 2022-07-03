e0f32b03abcf2a27fdff6635a5aa4e37b1544b7a
Yury <Jurystalker@mail.ru>
2022-07-03 17:18:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  function sum(number) {
+    let result = number;
+    function addition(s) {
+        result += s;
+        console.log(result);
+        return addition;
+    }
+    console.log(result);
+    return addition;
+  }
+  
+  sum(1);
+  sum(1)(2)(3)(99);
